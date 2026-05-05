18f31e6468d060ea57067a4db66ba72ad1926364
vendor-dev-fikran <fikran.vendor.pasti@pegadaian.co.id>
2026-05-05 22:33:55 +0700

    fix: add @astrojs/vercel adapter to fix 404 on /home route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  adapter: vercel(),
+  output: 'static',
   devToolbar: { enabled: false },
   integrations: [
     react(),
